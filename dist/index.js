4ea95d6820d27b445bdcd6fd1e65f807bb17440a
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2025-01-24 17:52:01 +0800

    Add TON-TESTNET token configuration

--- dist/index.js
+++ dist/index.js
@@ -2996,6 +2996,19 @@ define("@scom/scom-token-list/tonTokens/index.ts", ["require", "exports"], funct
                 address: 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs'
             }
         ].map(v => ({ ...v, chainId: 0, networkCode: 'TON' })),
+        'TON-TESTNET': [
+            {
+                name: 'Toncoin',
+                symbol: 'TON',
+                decimals: 9
+            },
+            {
+                name: 'USDT',
+                symbol: 'USDT',
+                decimals: 6,
+                address: 'kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy'
+            }
+        ].map(v => ({ ...v, chainId: 0, networkCode: 'TON-TESTNET' })),
     };
     exports.DefaultTokensByNetworkCode = DefaultTokensByNetworkCode;
 });
